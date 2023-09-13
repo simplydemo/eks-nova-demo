004f307404eb77b9fe29b35bf95b41eb554c0e30
symplesims <symplesims@gmail.com>
2023-09-14 02:34:13 +0900

    prometheus AMP 워크스페이스 쿼리 조회 Role 추가

--- plugins/prometheus/irsa.tf
+++ plugins/prometheus/irsa.tf
@@ -1,6 +1,8 @@
 locals {
   ingest_role_name   = format("%s%s", local.project, replace(title( format("%s-AmpIngestIRSARole", local.name) ), "-", "" ))
   ingest_policy_name = format("%s%s", local.project, replace(title( format("%s-AmpIngestIRSAPolicy", local.name) ), "-", "" ))
+  query_role_name    = format("%s%s", local.project, replace(title( format("%s-AmpQueryIRSARole", local.name) ), "-", "" ))
+  query_policy_name  = format("%s%s", local.project, replace(title( format("%s-AmpQueryIRSAPolicy", local.name) ), "-", "" ))
 }
 
 # ------------------------------------------
@@ -52,4 +54,54 @@ module "AmpIngest" {
   depends_on = [
     kubernetes_namespace_v1.prometheus
   ]
+}
+
+
+# ------------------------------------------
+# AMP Query Permissions
+# ------------------------------------------
+
+data "aws_iam_policy_document" "query" {
+  statement {
+    effect    = "Allow"
+    resources = [
+      "arn:aws:aps:${local.region_name}:${local.account_id}:workspace/*",
+      "arn:aws:aps:${local.region_name}:${local.account_id}:workspace/*/*",
+    ]
+
+    actions = [
+      "aps:ListWorkspaces",
+      "aps:GetLabels",
+      "aps:GetMetricMetadata",
+      "aps:GetSeries",
+      "aps:QueryMetrics",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "query" {
+  count       = var.enable_amazon_prometheus ? 1 : 0
+  name        = local.query_policy_name
+  description = "Set up the permission policy that grants query permissions for AMP workspace"
+  policy      = data.aws_iam_policy_document.query.json
+  tags        = merge(local.tags, {
+    Name = local.query_policy_name
+  })
+}
+
+module "AmpQuery" {
+  source = "../../irsa"
+
+  count = var.enable_amazon_prometheus ? 1 : 0
+
+  create_kubernetes_namespace = false
+  kubernetes_namespace        = local.namespace
+
+  kubernetes_service_account = "amp-query"
+  iam_role_name              = local.query_role_name
+  iam_policies_arn           = [aws_iam_policy.query[0].arn]
+  iam_role_path              = "/"
+  iam_permissions_boundary   = ""
+  cluster_name               = var.cluster_name
+  cluster_oidc_provider_arn  = var.irsa_config.cluster_oidc_provider_arn
 }
